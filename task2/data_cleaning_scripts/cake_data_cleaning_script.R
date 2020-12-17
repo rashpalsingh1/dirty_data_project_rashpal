0ff6392fda8573fa18786367e8dc88605e9f2593
rashpalsingh1 <74197844+rashpalsingh1@users.noreply.github.com>
2020-12-17 23:42:48 +0000

    task2 & task 4 completed

--- task2/data_cleaning_scripts/cake_data_cleaning_script.R
+++ task2/data_cleaning_scripts/cake_data_cleaning_script.R
@@ -10,97 +10,71 @@ library(janitor)
 cake_data <- read_csv("raw_data/cake-ingredients-1961.csv")
 cake_codes <- read_csv("raw_data/cake_ingredient_code.csv")
 
-#----------------------------------------------
-#check data dimension
-dim(cake_data)
 
-#check the variable names
-names(cake_data)
-#view the data
-view(cake_data)
-
-#--------------------------------------------------
-
+#make the data tidy, setting all variables as columns
 cake_data_longer <- cake_data %>% 
   pivot_longer(cols = c(AE:ZH),
                names_to = "ingredients",
                values_to = "amount")
                
-view(cake_data_longer)
-
+#remove NA values
 cake_data_simplified <- cake_data_longer %>% 
   drop_na()
 
+#create standardized column heading using clean_names
 clean_cake_data_simplified <- clean_names(cake_data_simplified)
-#view(clean_cake_data_simplified)
 
-#view(clean_cake_data_simplified)
-
-#view(cake_codes)
-
+#join the two tables to obtain the full names of ingredients
 cake_date_joined <- inner_join(clean_cake_data_simplified, cake_codes,
            by=c("ingredients" = "code"))
 
-view(cake_date_joined)
-
-#cake_date_joined$ingredients <- NULL
-
-nulled_cakes <- cake_date_joined %>% 
+#drop the codes column as it is no longer required
+final_cakes <- cake_date_joined %>% 
   select(-ingredients)
 
-view(nulled_cakes)
 
-new_order <- c("cake", "ingredient", "amount", "measure")
-
-
-rearranged_cakes <- nulled_cakes[, new_order]
-
-view(rearranged_cakes)
+write_csv(final_cakes, "clean_data/final_cakes.csv")
 
 #----------------------------------------
-#rearranged_cakes is the final version of the data this will
+#final_cakes is the final version of the data this will
 #be written to another file and used in the analysis part
 
 #1.Which cake has the most cocoa in it?
-rearranged_cakes %>% 
+final_cakes %>% 
   filter(ingredient == "Cocoa") %>%
   slice_max(amount, n = 1)
 
 #2. For sponge cake, how many cups of ingredients are used in total?
 
-rearranged_cakes %>%
+final_cakes %>%
   filter(measure == "cup" & cake == "Sponge") %>% 
   summarise(total = sum(amount))
 
 #3. How many ingredients are measured in teaspoons?
 
-rearranged_cakes %>% 
+final_cakes %>% 
   filter(measure == "teaspoon") %>% 
+  #need to count each ingredient once only
   distinct(ingredient) %>% 
   summarise(count = n()) 
 
+
  #4. Which cake has the most unique ingredients?
-cake_data_ingredient_counted <- rearranged_cakes %>% 
+ingredient_count <- final_cakes %>% 
   group_by(cake) %>% 
-    summarise(ingredient_count = n()) #%>% 
-    #arrange((ingredient_count))
-
-view (cake_data_ingredient_counted)
-
-cake_data_ingredient_counted %>%
-  group_by(cake) %>% 
-  summarise(
-    total = sum(ingredient_count)
-) %>% 
-  slice_min(total)
-  #arrange((total)) 
+    mutate(total = sum(ingredient_count = n())) 
+ingredient_count %>% 
+  summarise(cake_total = sum(total)) %>% 
+  arrange(cake_total)
 
 
 #5. Which ingredients are used only once?
 
-rearranged_cakes %>%
+final_cakes %>%
   group_by(ingredient) %>% 
   summarise(count = n()) %>%
   filter(count == 1)
 
-  
+
+
+
--- task4/data_cleaning_scripts/candy_cleaning_script.R
+++ task4/data_cleaning_scripts/candy_cleaning_script.R
@@ -2,15 +2,12 @@
 library(tidyverse)
 library(janitor)
 library(readxl)
+library(openxlsx)
 
 candy_data_2015 <- read_excel("raw_data/boing-boing-candy-2015.xlsx")
 candy_data_2016 <- read_excel("raw_data/boing-boing-candy-2016.xlsx")
 candy_data_2017 <- read_excel("raw_data/boing-boing-candy-2017.xlsx")
 
-
-view(candy_data_2015)
-view(candy_data_2016)
-view(candy_data_2017)
 
 #rename columns needed for data analysis in 2015 data
 candy_data_2015_1 <- candy_data_2015 %>%
@@ -18,21 +15,19 @@ candy_data_2015_1 <- candy_data_2015 %>%
   rename("trick_or_treat" = 
            "Are you going actually going trick or treating yourself?") %>%
   #added a column for country, year & gender
-  mutate(country = "Unknown") %>% 
+  mutate(country = NA) %>% 
   mutate(year = "2015") %>% 
-  mutate(gender = "Unknown")
+  mutate(gender = NA)
 
-#view(candy_data_2015_1)
-
-candy_test <- candy_data_2015_1 %>%
+#select the appropriate columns for '15
+candy_data_2015_2 <- candy_data_2015_1 %>%
   select(age, trick_or_treat, country, year, gender, starts_with("["))
 
-candy_test_longer <- candy_test %>% 
+#make the data tidy - setting all variables as columns (sweets as a column)
+candy_data_2015_longer <- candy_data_2015_2 %>% 
   pivot_longer(cols = c(starts_with("[")),
                names_to = "sweets",
                values_to = "opinion")
-
-#view(candy_test_longer)
 
 
 #rename columns needed for data analysis in 2016 data
@@ -44,36 +39,28 @@ candy_data_2016_1 <- candy_data_2016 %>%
   rename("country" = "Which country do you live in?") %>% 
   mutate(year = "2016")
   
-#select the approraite columns from 2016 data
-candy_data_2016_test <- candy_data_2016_1 %>%
+#select the appropriate columns from 2016 data
+candy_data_2016_2 <- candy_data_2016_1 %>%
   select(age, trick_or_treat, country, year, gender, starts_with("["))
 
-#view(candy_data_2016_test)
 
 #make the data tidy - setting all variables as columns (sweets as a column)
-candy_data_2016_test_longer <- candy_data_2016_test %>% 
+candy_data_2016_longer <- candy_data_2016_2 %>% 
   pivot_longer(cols = c(starts_with("[")),
                names_to = "sweets",
                values_to = "opinion")
 
-#view(candy_data_2016_test_longer)
-
-candy_data_combined_15_16 <- rbind(candy_test_longer, 
-                                   candy_data_2016_test_longer) 
+#combine the data for '15 & '16
+candy_data_combined_15_16 <- bind_rows(candy_data_2015_longer, 
+                                   candy_data_2016_longer) 
 
 #remove the '[]' around the sweet names for 15-16 data
 final_data_15_16 <- candy_data_combined_15_16 %>%
   mutate(sweets = str_remove_all(sweets, "\\[")) %>% 
   mutate(sweets = str_remove_all(sweets, "\\]"))
   
-#view(final_data_15_16)
 
-
-#view(candy_data_combined_15_16)
-#write_excel_csv(candy_data_combined_15_16, "raw_data/excel_test2.csv")
-
-
-#rename 2017 columns
+#rename columns needed for data analysis in 2017 data
 candy_data_2017_1 <- candy_data_2017 %>%
   rename("age" = "Q3: AGE") %>%
   rename("trick_or_treat" = 
@@ -83,24 +70,23 @@ candy_data_2017_1 <- candy_data_2017 %>%
   #add a column for year
   mutate(year = "2017")
 
-#select the appropriate columns from 2016 data
-candy_data_2017_test <- candy_data_2017_1 %>%
+#select the appropriate columns from 2017 data
+candy_data_2017_2 <- candy_data_2017_1 %>%
   select(age, trick_or_treat, country, year, gender, starts_with("Q6"))
 
 #make the data tidy - setting all variables as columns (sweets as a column)
-candy_data_2017_test_longer <- candy_data_2017_test %>% 
+candy_data_2017_longer <- candy_data_2017_2 %>% 
   pivot_longer(cols = c(starts_with("Q6")),
                names_to = "sweets",
                values_to = "opinion")
 
 #remove the "Q6" from the sweet names
-final_data_17 <- candy_data_2017_test_longer %>% 
+final_data_17 <- candy_data_2017_longer %>% 
   mutate(sweets = str_remove_all(sweets, "Q6 \\| "))
 
 #combine the data from all 3 years
-candy_data_combined_15_16_17 <- rbind(final_data_15_16, 
+candy_data_combined_15_16_17 <- bind_rows(final_data_15_16, 
                                       final_data_17)
-#view(candy_data_combined_15_16_17)
 
 
 #extract only numeric characters from age column
@@ -108,24 +94,46 @@ candy_data_combined_15_16_17 <- candy_data_combined_15_16_17 %>%
   mutate(age = str_extract(age, "[0-9]?[0-9]?[0-9]?")) %>% 
   mutate(age = as.numeric(age)) %>% 
   #only include ages between 0-99 (I assume babies get 
-  #counted whilst parents eat candy on their behalf)
+  #counted whilst parents eat candy on their behalf!)
  mutate(age = ifelse(age > 99, NA, age))
 
+#remove ambiguously specified gender category
+candy_data_combined_15_16_17 <- candy_data_combined_15_16_17 %>% 
+  mutate(gender = ifelse(gender =="I'd rather not say", NA, gender))
+
    
+#add columns to convert opinions to numeric values for Qs 5,6,7
+candy_data_combined_15_16_17 <- candy_data_combined_15_16_17 %>%
+  mutate(opinion_int = ifelse(opinion == "JOY", 1,
+  ifelse(opinion == "DESPAIR", -1, 
+  ifelse(opinion == "MEH", 0, opinion)))) %>% 
+  mutate(opinion_int = as.numeric(opinion_int))
 
 
- 
-
-
-
-
-
-
-#write_csv(str_test, "raw_data/str_test11.csv")
-
-
-#tail(str_test)
-#df %>% filter(!grepl("^1", y))
+#create new country column to contain a subset of nations - only America, UK, Canada & other
+candy_data_final <- candy_data_combined_15_16_17 %>%
+  mutate(country_subset = ifelse(str_detect(country, "[Aa][Mm][Ee][Rr][Ii][Cc][Aa]"), "america",
+                ifelse(str_detect(country, "[Uu][Ss][Aa]"), "america",
+                ifelse(str_detect(country, "[Ss][Tt][Aa][Tt][Ee]"), "america",
+                ifelse(str_detect(country, "[Uu][Nn][Ii][Tt][Ee][Dd] [Ss]"), "america", 
+                ifelse(str_detect(country, "[Uu][Ss]"), "america",
+                ifelse(str_detect(country, "[Uu][:punct:][Ss][:punct:][Aa]*"), "america",
+                ifelse(str_detect(country, "[Uu][:punct:][Ss][:punct:]"), "america",
+                ifelse(str_detect(country, "[Uu] [Ss] "), "america",
+                ifelse(str_detect(country, "merica"), "america",
+                ifelse(str_detect(country, "[Uu] [Ss]"), "america",
+                ifelse(str_detect(country, "Yoo Ess"), "america",
+                ifelse(str_detect(country, "Trump"), "america",
+                ifelse(str_detect(country, "[:punct:][Aa][Mm][Ee][Rr][Ii][Cc][Aa]"), "america",
+                ifelse(str_detect(country, "[Cc][Aa][Nn]"), "canada",
+                ifelse(str_detect(country, "[Uu][Nn][Ii][Tt][Ee][Dd] [Kk]"), "UK",
+                ifelse(str_detect(country, "[Uu][:punct:][Kk]"), "UK",
+                ifelse(str_detect(country, "[Ee][Nn][Gg][Ll][Aa][Nn][Dd]"), "UK",
+                ifelse(str_detect(country, "[Ss][Cc][Oo][Tt][Ll][Aa][Nn][Dd]"), "UK", "other"
+                                 )))))))))))))))))))
+                          
+#final version of data saved to separate folder
+write.xlsx(candy_data_final, "clean_data/candy_data_final3.xlsx")
 
 
 
@@ -136,7 +144,7 @@ candy_data_combined_15_16_17 <- candy_data_combined_15_16_17 %>%
 #(number of candy ratings, not number of raters. Don’t count missing values)
 
 #returns the number of opinions given which are not NA
-candy_data_combined_15_16_17 %>%
+candy_data_final %>%
     filter(!is.na(opinion)) %>% 
     summarise(count = n())
 
@@ -144,106 +152,79 @@ candy_data_combined_15_16_17 %>%
 #and the average age of people not going trick or treating?
 
 #answer when going out trick or treating
-candy_data_q2_yes <- candy_data_combined_15_16_17 %>%
-  filter(trick_or_treat == "Yes")
+candy_data_final %>%
+  filter(trick_or_treat == "Yes") %>% 
+  summarise(average_age = median(age, na.rm = TRUE))
 
-candy_data_q2_yes <- candy_data_q2_yes %>%
-  mutate(average_age = median(age, na.rm = TRUE))
-head(candy_data_q2_yes, 1)
 
 #answer when not going out trick or treating
-candy_data_q2_no <- candy_data_combined_15_16_17 %>%
-  filter(trick_or_treat == "No")
-
-candy_data_q2_no <- candy_data_q2_no %>%
-  mutate(average_age = median(age, na.rm = TRUE))
-  head(candy_data_q2_no, 1)
+candy_data_final %>%
+  filter(trick_or_treat == "No") %>% 
+  summarise(average_age = median(age, na.rm = TRUE))
 
   
 #3. For each of joy, despair and meh, 
 #which candy bar revived the most of these ratings?
-  #for Joy:
-  
- # candy_data_combined_15_16_17 %>% 
-#    select(sweets, opinion) %>% 
-#   filter(grepl("bar", sweets)) %>% 
-#  filter(!is.na(opinion)) %>% 
-#  group_by(opinion, sweets) %>% 
-#  summarise(count = n()) %>% 
-#  filter(count == max(count))
-  
-  candy_data_combined_15_16_17 %>% 
+
+candy_data_final %>% 
     filter(grepl("[Bb][Aa][Rr]", sweets)) %>% 
-    group_by(sweets, opinion) %>% 
+    filter(!is.na(opinion)) %>% 
+    group_by(opinion, sweets) %>% 
     summarise(count = n()) %>% 
-    arrange(desc(count)) %>% 
-    filter(opinion == "JOY") %>% 
-    head(1)
-  
-  #for Despair:
-  candy_data_combined_15_16_17 %>%
-    filter(grepl("[Bb][Aa][Rr]", sweets)) %>% 
-    group_by(sweets, opinion) %>% 
-    summarise(count = n()) %>% 
-    arrange(desc(count)) %>% 
-    filter(opinion == "DESPAIR") %>% 
-    head(1)
-  
-  #for Meh:
-  candy_data_combined_15_16_17 %>%
-    filter(grepl("[Bb][Aa][Rr]", sweets)) %>% 
-    group_by(sweets, opinion) %>% 
-    summarise(count = n()) %>% 
-    arrange(desc(count)) %>% 
-    filter(opinion == "MEH") %>% 
-    head(1)
+    filter(count == max(count)) %>% 
+    arrange(desc(count))
+   
   
   #4. How many people rated Starburst as despair?
   
-  candy_data_combined_15_16_17 %>%
+candy_data_final %>%
     filter(grepl("[Ss][Tt][Aa][Rr][Bb][Uu][Rr][Ss][Tt]", sweets)) %>%
     filter(opinion == "DESPAIR") %>%
     summarise(count = n())
   
   
+  #
+  #Where is question5????????
+  #
+  
+  #6.What was the most popular candy bar by this 
+  #rating system for each gender in the dataset?
+  
+
+candy_data_final %>%
+    filter(!is.na(gender)) %>% 
+    filter(grepl("[Bb][Aa][Rr]", sweets)) %>%
+    filter(!is.na(opinion_int)) %>%
+    group_by(gender, sweets) %>% 
+    summarise(popularity = sum(opinion_int)) %>% 
+    filter(popularity == max(popularity)) %>% 
+    arrange(desc(popularity))
+    
+  
+  #7. What was the most popular candy bar in each year?
+candy_data_final %>%
+    filter(grepl("[Bb][Aa][Rr]", sweets)) %>%
+    filter(!is.na(opinion_int)) %>%
+    group_by(year, sweets) %>% 
+    summarise(popularity = sum(opinion_int)) %>% 
+    filter(popularity == max(popularity)) %>% 
+    arrange(desc(year))
   
   
+  #8. What was the most popular candy bar by this rating for people in 
+  #US, Canada, UK and all other countries?
   
+candy_data_final %>%
+    filter(!is.na(country_subset)) %>% 
+    filter(grepl("[Bb][Aa][Rr]", sweets)) %>%
+    filter(!is.na(opinion_int)) %>%
+    group_by(country_subset, sweets) %>% 
+    summarise(popularity = sum(opinion_int)) %>% 
+    filter(popularity == max(popularity)) %>% 
+    arrange(desc(popularity))
   
-#candy_data_combined_15_16_17 %>% 
-#  filter(trick_or_treat == "No" | trick_or_treat == "Yes") %>% 
- # group_by(trick_or_treat) %>% 
-#  summarise(
- #   mutate(average_age = median(age, na.rm = TRUE))
-#  ) 
-
-#write_excel_csv(filtered, "raw_data/filtered2.csv")
-
-#filtered2 <- candy_data_combined_15_16_17 %>%
- # filter(grepl("^[a-z]+", age))
-
-#view(filtered2)
-#write_excel_csv(filtered2, "raw_data/grepl.csv")
-
-
-
-#=======================================================
-write_excel_csv(candy_data_combined_15_16_17, 
-               "raw_data/final_data.csv")
-
-#=======================================================
-
-
-
-#more clean up on combined data
 
 
 
 
-#-----------------------------------------
-#Graham A code test
-#filtered <- candy_data_2016_1 %>% 
- # filter(is.na(age))
 
-#view(filtered)
-#-------------------------------------------
